0f37e0351e60c39167c9711d16db66042d8b3a7a
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-09-08 07:35:14 -0300

    Implementado o método CalcularDistanciaEntreDoisPontos.

--- src/br/com/CalculoDeCordenadas/aplicacao/Programa.java
+++ src/br/com/CalculoDeCordenadas/aplicacao/Programa.java
@@ -6,8 +6,25 @@ import br.com.CalculoDeCordenadas.dominio.Ponto;
         public static void main(String [] args)
         {
             Scanner ler = new Scanner(System.in);
+                Ponto pontoA = new Ponto();
+
+                 Ponto pontoB = new Ponto();
+
+                 System.out.println("Informe a cordenada X  para o ponto A");
+                    pontoA.setX(ler.nextDouble());
+
+                    System.out.println("Informe a cordenada y  para o ponto A");
+                        pontoA.setY(ler.nextDouble());
+
+            System.out.println("Agora vamos informar o ponto B");
+
+                            System.out.println("Informe a cordenada X  para o ponto B");
+                                            pontoB.setX(ler.nextDouble());
+
+                                                System.out.println("Informe a cordenada y  para o ponto B");
+                                                     pontoB.setY(ler.nextDouble());
 
 
-
+                System.out.println("A distancia entre os pontos A e B é: "+ pontoA.CalcularDistanciaEntreDoisPontos(pontoA,pontoB));
         }
     }
--- src/br/com/CalculoDeCordenadas/dominio/Ponto.java
+++ src/br/com/CalculoDeCordenadas/dominio/Ponto.java
@@ -40,4 +40,12 @@ package br.com.CalculoDeCordenadas.dominio;
                 return "(" + x + "," + y + ")";
             }
 
+            public double CalcularDistanciaEntreDoisPontos(Ponto pontoA, Ponto pontoB)
+                    //https://brasilescola.uol.com.br/matematica/distancia-entre-dois-pontos.htm
+            {
+                var distancia = Math.sqrt(Math.pow(((pontoB.x) - (pontoA.x)), 2) + Math.pow(((pontoB.y) - (pontoA.y)), 2));
+
+                return distancia;
+            }
+
     }
