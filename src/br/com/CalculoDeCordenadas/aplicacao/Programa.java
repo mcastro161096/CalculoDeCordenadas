78dac9482f6b2b7a71157791253fdac69da7b47d
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-09-08 06:31:33 -0300

    Adicionada a classe Programa.

---
+++ src/br/com/CalculoDeCordenadas/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package br.com.CalculoDeCordenadas.aplicacao;
+import java.util.Scanner;
+import br.com.CalculoDeCordenadas.dominio.Ponto;
+    public class Programa
+    {
+        public static void main(String [] args)
+        {
+            Scanner ler = new Scanner(System.in);
+
+
+
+        }
+    }
